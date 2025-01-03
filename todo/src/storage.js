7ff4e81818fd482c7445fbb53a440e6df5b3ec3f
CodingSwordfish <wagobryan@gmail.com>
2025-01-03 06:51:17 +0000

    Add local storage to handle saving the added folders persistently

---
+++ todo/src/storage.js
@@ -0,0 +1,5 @@
+export const getProjects = () => JSON.parse(localStorage.getItem('projects')) || [];
+
+export const saveProjects = (projects) => {
+    localStorage.setItem('projects', JSON.stringify(projects));
+};
